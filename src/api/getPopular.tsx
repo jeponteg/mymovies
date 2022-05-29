1105bd63647bc72e24b5c07e3d47ed0c234f6388
javier ponte <javierp@MacBook-Pro-de-javier.local>
2022-05-29 12:19:00 -0400

    creando componente Âpara peliculas populares

--- src/api/getPopular.tsx
+++ src/api/getPopular.tsx
@@ -3,5 +3,5 @@ import { MovieDBNowPlaying  } from '../interfaces/moviesInterface'
 
 const API_KEY = 'b4ccf34d74031aa55f125a731bc7544e';
 
-export const getPopular = async (page=1) => await axiosInstances.get<MovieDBNowPlaying >(`/movie/popular?api_key=${API_KEY}&page=${page}`).catch((console.log))
+export const getPopular = async (page=1) => await axiosInstances.get<MovieDBNowPlaying>(`/movie/popular?api_key=${API_KEY}&page=${page}`).catch((console.log))
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,11 @@
 import React, { FC, useContext, useEffect, useState } from 'react'
 import { MovieContext } from '../context/MoviesProvider'
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, Dimensions } from 'react-native';
+
+import Carousel from 'react-native-snap-carousel';
+import MoviePoster from '../components/MoviePoster';
+
+const windowWidth = Dimensions.get('window').width;
 
 const Home:FC = () => {
 
@@ -12,9 +17,26 @@ const Home:FC = () => {
 
     return(
         
-       null
-      
+        <View style={{marginTop: 20}}>
+            <View style={{height:440}} >
+                <Carousel
+                    data={popular}
+                    renderItem={ ({ item }:any) =>  <MoviePoster movie={ item }/>} 
+                    sliderWidth = {windowWidth}
+                    itemWidth={300}
+                />
+            </View>
 
+            <View style={{height:230}} >
+                <Text style={{fontWeight:'bold', fontSize:20}} >Populares</Text>
+                <FlatList 
+                    data={popular}
+                    renderItem={({item} :any) => <MoviePoster movie={ item} /> }
+                    keyExtractor={(item) => item.id.toString()}
+                    horizontal={true}
+                />
+            </View>
+        </View>
     )
 }
 
